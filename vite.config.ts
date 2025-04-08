47e90e1cb71c6b26ba764297828af754c191a001
nima boustanian <nima.boustanian@gmail.com>
2025-04-08 15:14:34 +0200

    demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,32 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  build: {
-    lib: {
-      // Entry point for the library
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'AdvancedFrontendLogger',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs', 'umd']
-    },
-    rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
-      external: [],
-      output: {
-        // Provide global variables to use in the UMD build
-        globals: {},
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    // Development mode - just serve the demo directory
+    return {
+      root: 'demo',
+      server: {
+        open: true
+      }
+    };
+  } else {
+    // Build mode - build the library
+    return {
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/index.ts'),
+          name: 'AdvancedFrontendLogger',
+          fileName: (format) => `index.${format}.js`
+        },
+        sourcemap: true
       },
-    },
-    // Don't use the default index.html as entry
-    emptyOutDir: true, 
-    sourcemap: true,
-    minify: 'terser',
-  },
-  plugins: [
-    dts({
-      include: ['src/**/*.ts'],
-      rollupTypes: true,
-    }),
-  ],
+      plugins: [
+        dts({
+          include: ['src/**/*.ts'],
+          rollupTypes: true
+        })
+      ]
+    };
+  }
 });
