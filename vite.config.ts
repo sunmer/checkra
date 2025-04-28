af63e61774827c2fd529e729d921f137ecaba558
nima boustanian <nima.boustanian@gmail.com>
2025-04-28 11:41:39 +0200

    build process for instructions.html

--- vite.config.ts
+++ vite.config.ts
@@ -85,7 +85,8 @@ export default defineConfig(({ command, mode }) => {
           rollupOptions: {
             input: {
               main: resolve(__dirname, 'demo/index.html'),
-              privacy: resolve(__dirname, 'demo/privacy.html')
+              privacy: resolve(__dirname, 'demo/privacy.html'),
+              setup: resolve(__dirname, 'demo/setup.html') 
             }
           }
         },
